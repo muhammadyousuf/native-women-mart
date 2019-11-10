e62af44e947a5fdebcf364c36357072a72489eb4
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-11-10 23:19:19 +0500

    add theme color

--- scr/components/HeaderComponent/index.js
+++ scr/components/HeaderComponent/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {StyleSheet,  SafeAreaView, StatusBar} from 'react-native';
-import {Header, Left, Body, Right, Button, Icon, Title} from 'native-base';
-
+import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import {Header, Left, Body, Right, Button, Icon, Title, Subtitle} from 'native-base';
+import {HeaderThemeColor} from '../../themes/color';
 class HeaderComponent extends React.Component {
   render() {
     const {navigation} = this.props;
     return (
       <SafeAreaView style={styles.container}>
-        <Header>
-          <Left>
+        <Header
+          style={{backgroundColor: HeaderThemeColor}}
+          androidStatusBarColor={HeaderThemeColor} >
+          <Left style={{flex: 1}}>
             <Button onPress={() => navigation.toggleDrawer()} transparent>
               <Icon
                 type="MaterialCommunityIcons"
@@ -17,12 +19,18 @@ class HeaderComponent extends React.Component {
               />
             </Button>
           </Left>
-          <Body>
-            <Title>Women Shop</Title>
+          <Body
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Title style={{fontFamily:'times'}} >Women</Title>
+            <Subtitle style={{fontFamily:'times'}} >---Shop---</Subtitle>
           </Body>
           <Right>
             <Button transparent>
-              <Icon name="close" />
+              <Icon
+                name="shopping-cart"
+                type="FontAwesome"
+                style={{fontSize: 30}}
+              />
             </Button>
           </Right>
         </Header>
