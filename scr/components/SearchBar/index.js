05d8205ec86e0300f924dec22491dbdb7668204a
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-21 20:33:32 +0500

    modal open

--- scr/components/SearchBar/index.js
+++ scr/components/SearchBar/index.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Modal,
+  TouchableHighlight,
+  Alert,
+} from 'react-native';
 import {GrayColor, BgThemeColor} from '../../themes/color';
 import {UIThemeFont} from '../../themes/fonts';
-import {Icon, Text} from 'native-base';
+import {Icon, Text, Item, Input} from 'native-base';
 class SearchBar extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
   render() {
     return (
-      <TouchableOpacity style={styles.SearchStyle}>
-        <Icon style={styles.IconStyle} name="ios-search" />
-        <Text style={styles.textStyles}>Search Entire Women Shop</Text>
-      </TouchableOpacity>
+      <>
+        <TouchableOpacity
+          style={styles.SearchStyle}
+          onPress={() => this.setState({modalVisible: true})}>
+          <Icon style={styles.IconStyle} name="ios-search" />
+          <Text style={styles.textStyles}>Search Entire Women Shop</Text>
+        </TouchableOpacity>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 2}}>
+            <View>
+              <Item>
+                <Input placeholder="What are you looking for..." />
+              </Item>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+      </>
     );
   }
 }
@@ -29,12 +69,12 @@ const styles = StyleSheet.create({
     marginLeft: '5%',
     marginTop: '4%',
     marginBottom: '5%',
-    borderWidth:2,
+    borderWidth: 2,
     height: 40,
     borderColor: BgThemeColor,
     borderRadius: 8,
     flexDirection: 'row',
-    backgroundColor:GrayColor,
+    backgroundColor: GrayColor,
   },
   textStyles: {
     marginTop: 10,
