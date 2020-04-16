57d8d706d0bb5c532043b98e0915bc9181ff29ac
dadaniyalamjad <dadaniyal.amjad@yahoo.com>
2020-04-17 00:05:02 +0500

    ios setting in app

--- App.js
+++ App.js
@@ -2,6 +2,28 @@ import React from 'react';
 import {StyleSheet, StatusBar} from 'react-native';
 import AppContainer from './scr/navigation/stackNavigation';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import EntypoIcons from 'react-native-vector-icons/Entypo';
+import EvilIconsIcons from 'react-native-vector-icons/EvilIcons';
+import FeatherIcons from 'react-native-vector-icons/Feather';
+import fontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import FontistoIcons from 'react-native-vector-icons/Fontisto';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialComunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import IonIcons from 'react-native-vector-icons/Ionicons';
+import Octicons from 'react-native-vector-icons/Octicons';
+
+AntDesignIcons.loadFont();
+EntypoIcons.loadFont();
+EvilIconsIcons.loadFont();
+FeatherIcons.loadFont();
+fontAwesomeIcons.loadFont();
+Octicons.loadFont();
+
+FontistoIcons.loadFont();
+MaterialIcons.loadFont();
+MaterialComunityIcons.loadFont();
+IonIcons.loadFont();
 
 const App = () => {
   return (
