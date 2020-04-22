0efe7fc8f21fe8ff4f4856a4891528e483233f26
Daniyal Amjad <dadaniyal.amjad@yahoo.com>
2020-04-22 14:11:17 +0500

    Add Splash Screen

---
+++ scr/screens/Splash.js
@@ -0,0 +1,29 @@
+import React, {useEffect} from 'react';
+import {View, Image, StyleSheet, Text} from 'react-native';
+import Logo from '../assets/images/logo.png';
+import {BgThemeColor} from '../themes/color';
+
+const Splash = props => {
+  useEffect(() => {
+    setTimeout(() => {
+      props.navigation.navigate('Home');
+    }, 3000);
+  }, null);
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.ProfileImage} />
+    </View>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: BgThemeColor,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ProfileImage: {width: 180, height: 180, borderRadius: 180 / 2},
+});
