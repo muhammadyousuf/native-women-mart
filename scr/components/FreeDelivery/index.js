1dbb3706edc05fdf27d97584e72a8ed51e44641d
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-07-19 19:32:56 +0500

    change class component to function component

--- scr/components/FreeDelivery/index.js
+++ scr/components/FreeDelivery/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import {StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
 import {WhiteBgColor} from '../../themes/color';
 import {UIThemeFont} from '../../themes/fonts';
-class FreeDelivery extends React.Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <TouchableOpacity>
-          <Text style={styles.textName}>
-            Free Delivery On Order Above RS 5000
-          </Text>
-        </TouchableOpacity>
-      </SafeAreaView>
-    );
-  }
-}
+const FreeDelivery = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity>
+        <Text style={styles.textName}>Free Delivery On Order Above RS 500</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
 export default FreeDelivery;
 
