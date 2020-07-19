62067bc8364d12de3a954cbf4521431f30770c1d
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-07-19 21:16:03 +0500

    header component change hooks

--- scr/components/HeaderComponent/index.js
+++ scr/components/HeaderComponent/index.js
@@ -13,48 +13,47 @@ import {
 import {BgThemeColor, StatusBarColor, WhiteBgColor} from '../../themes/color';
 import {UIThemeFont} from '../../themes/fonts';
 
-class HeaderComponent extends React.Component {
-  card = () => {
-    this.props.navigation.navigate('Login');
+const HeaderComponent = props => {
+  const card = () => {
+    props.navigation.navigate('Login');
   };
-  render() {
-    const {navigation} = this.props;
-    return (
-      <View style={styles.container}>
-        <Header
-          style={{backgroundColor: BgThemeColor}}
-          androidStatusBarColor={StatusBarColor}>
-          <Left style={styles.leftStyle}>
-            <Button onPress={() => navigation.toggleDrawer()} transparent>
-              <Icon
-                type="MaterialCommunityIcons"
-                style={styles.iconStyle}
-                name="menu"
-              />
-            </Button>
-          </Left>
-          <Body style={styles.bodyStyle}>
-            {this.props.navigation.state.routeName === 'Home' ? (
-              <View>
-                <Title style={styles.headerBodyStyle}>Women</Title>
-                <Subtitle style={styles.headerBodyStyle}>---Shop---</Subtitle>
-              </View>
-            ) : (
-              <Title style={styles.headerBodyStyle}>
-                {this.props.navigation.state.routeName}
-              </Title>
-            )}
-          </Body>
-          <Right>
-            <Button transparent onPress={this.card}>
-              <Icon name="bell-o" type="FontAwesome" style={styles.iconStyle} />
-            </Button>
-          </Right>
-        </Header>
-      </View>
-    );
-  }
-}
+
+  const {navigation} = props;
+  return (
+    <View style={styles.container}>
+      <Header
+        style={{backgroundColor: BgThemeColor}}
+        androidStatusBarColor={StatusBarColor}>
+        <Left style={styles.leftStyle}>
+          <Button onPress={() => navigation.toggleDrawer()} transparent>
+            <Icon
+              type="MaterialCommunityIcons"
+              style={styles.iconStyle}
+              name="menu"
+            />
+          </Button>
+        </Left>
+        <Body style={styles.bodyStyle}>
+          {props.navigation.state.routeName === 'Home' ? (
+            <View>
+              <Title style={styles.headerBodyStyle}>Women</Title>
+              <Subtitle style={styles.headerBodyStyle}>---Shop---</Subtitle>
+            </View>
+          ) : (
+            <Title style={styles.headerBodyStyle}>
+              {navigation.state.routeName}
+            </Title>
+          )}
+        </Body>
+        <Right>
+          <Button transparent onPress={card}>
+            <Icon name="bell-o" type="FontAwesome" style={styles.iconStyle} />
+          </Button>
+        </Right>
+      </Header>
+    </View>
+  );
+};
 
 export default HeaderComponent;
 
