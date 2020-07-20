b5615db7cbaa376c00260502e8736d5b9674bcf4
muhammad yousuf <muhammadyousuf327@gmail.com>
2020-07-21 01:30:26 +0500

    new picture zoom package add

--- scr/screens/cardItem.js
+++ scr/screens/cardItem.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import ImageView from 'react-native-image-viewing';
+const clothes3 = require('../assets/images/clothes3.webp');
+const images = [
+  clothes3,
+  {
+    uri: 'https://images.unsplash.com/photo-1573273787173-0eb81a833b34',
+  },
+];
 
 const CardItemScreen = props => {
+  const [visible, setIsVisible] = useState(false);
   const {goBack} = props.navigation;
   return (
     <View style={styles.container}>
-      <Text>Card Item Page</Text>
+      <TouchableOpacity onPress={() => setIsVisible(true)}>
+        <Image style={{width: 300, height: 200}} source={clothes3} />
+      </TouchableOpacity>
+      <ImageView
+        images={images}
+        imageIndex={0}
+        visible={visible}
+        onRequestClose={() => setIsVisible(false)}
+      />
       <Button title="go to home" onPress={() => goBack()} />
     </View>
   );
@@ -15,7 +39,7 @@ export default CardItemScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    //flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 40,
